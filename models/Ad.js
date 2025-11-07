c7188dbbb3f8113ad8b6fc43bdcbcace8e410586
zieevruslan8-dot <zieevruslan8@gmail.com>
2025-11-08 01:57:27 +0500

    Create Ad.js

---
+++ models/Ad.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const adSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  imageUrl: String,
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Ad', adSchema);
