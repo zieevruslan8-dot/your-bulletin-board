d9a4028e602b8764ab8b0e3f1904f3298b0f7f4b
zieevruslan8-dot <zieevruslan8@gmail.com>
2025-11-08 01:53:54 +0500

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
